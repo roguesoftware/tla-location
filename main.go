77e911fc7ed40bf84a6d86243e2c9793d4aff4c7
Brian Martin <bri365@gmail.com>
2020-06-14 22:36:13 -0500

    load initial values from json file

--- main.go
+++ main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 
 	pb "github.com/roguesoftware/tla-proto"
 )
 
-const (
-	port = ":50505"
-)
+const port = ":50505"
+
+var initialLocations []*pb.LocationItem
 
 type server struct {
 	pb.UnimplementedLocationServiceServer
@@ -23,42 +26,35 @@ func (s *server) GetLocations(ctx context.Context, in *pb.LocationRequest) (*pb.
 	lat := in.GetLatitude()
 	rad := in.GetRadius()
 
-	// log.Printf("Received: %v %v %v", lon, lat, rad)
 	log.Printf("Received: %v %v %v", lon, lat, rad)
 
 	var locations []*pb.LocationItem
-	var location pb.LocationItem
-
-	location.Id = "abcdef"
-	location.Longitude = -112.5
-	location.Latitude = 32.3
-	location.Address = "123 Main St, Leander, TX 78641"
-	location.Title = "City Hall"
-	location.Distance = 10468.4
-
-	locations = append(locations, &location)
-
-	location.Id = "123456-fjkghi"
-	location.Longitude = -112.3
-	location.Latitude = 32.5
-	location.Address = "456 South St, Leander, TX 78641"
-	location.Title = "Water Tower"
-	location.Distance = 10468.4
-
-	locations = append(locations, &location)
+	locations = initialLocations[1:2]
 
 	return &pb.LocationReply{Locations: locations}, nil
 }
 
 func main() {
+	// load initial locations
+	fileName := "locations.json"
+	jsonFile, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalf("Error opening %v: %v", fileName, err)
+	}
+	defer jsonFile.Close()
+
+	jsonBytes, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(jsonBytes, &initialLocations)
+
+	// create listener
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("Failed to listen: %v", err)
 	}
 
 	s := grpc.NewServer()
 	pb.RegisterLocationServiceServer(s, &server{})
-	log.Printf("Registered location server")
+	log.Printf("Registered location server with %v stories", len(initialLocations))
 	if err := s.Serve(lis); err != nil {
 		log.Fatal(s.Serve(lis))
 	}
